e708a97a86b2a334d2690ed7ff4a1be810ff5551
Anton Sergunov <setosha@gmail.com>
2025-06-07 11:56:24 +0600

    make it work with non-modified can-bus

--- components/can_logger/can_logger.cpp
+++ components/can_logger/can_logger.cpp
@@ -56,6 +56,7 @@ void CanLogger::setup() {
         print_can_packet(file, true, log_id, can_id, extended_id, rtr, data);
         fprintf(file, "\n");
       });
+#ifdef USE_CANBUS_TX_CALLBACK
       bus.bus->add_transmit_callback([this, log_id = bus.log_id](
                                          uint32_t can_id, bool extended_id,
                                          bool rtr,
@@ -65,29 +66,35 @@ void CanLogger::setup() {
         print_can_packet(file, false, log_id, can_id, extended_id, rtr, data);
         fprintf(file, "\n");
       });
-#ifdef USE_ESP32_CAN
-      bus.bus->add_config_callback([this, log_id = bus.log_id](
-                                       const twai_general_config_t &g_config,
-                                       const twai_timing_config_t &t_config,
-                                       const twai_filter_config_t &f_config,
-                                       esp_err_t err) {
-        auto &&file = this->open_();
-        print_timestamp(file);
-        print_can_command(file, log_id, "CBC");
-        if (t_config.quanta_resolution_hz) {
-          fprintf(file, " speed=%" PRIu32 " quanta_resolution_hz=%" PRIu32 " tseg_1=%d tseg_2=%d", 
-            t_config.quanta_resolution_hz/(1+t_config.tseg_1 + t_config.tseg_2), t_config.quanta_resolution_hz, t_config.tseg_1, t_config.tseg_2);
-        }
-        fprintf(file,
-                " tx_io=%i rx_io=%i clkout_io=%i bus_off_io=%i "
-                "tx_queue_len=%" PRIu32 " rx_queue_len=%" PRIu32
-                " alerts_enabled=%" PRIu32 " clkout_divider=%" PRIu32
-                " intr_flags=0x%X\n",
-                g_config.tx_io, g_config.rx_io, g_config.clkout_io,
-                g_config.bus_off_io, g_config.tx_queue_len,
-                g_config.rx_queue_len, g_config.alerts_enabled,
-                g_config.clkout_divider, g_config.intr_flags);
-      });
+#endif
+#ifdef USE_ESP32_CAN_CONFIG_CALLBACK
+      bus.bus->add_config_callback(
+          [this, log_id = bus.log_id](const twai_general_config_t &g_config,
+                                      const twai_timing_config_t &t_config,
+                                      const twai_filter_config_t &f_config,
+                                      esp_err_t err) {
+            auto &&file = this->open_();
+            print_timestamp(file);
+            print_can_command(file, log_id, "CBC");
+            if (t_config.quanta_resolution_hz) {
+              fprintf(file,
+                      " speed=%" PRIu32 " quanta_resolution_hz=%" PRIu32
+                      " tseg_1=%d tseg_2=%d",
+                      t_config.quanta_resolution_hz /
+                          (1 + t_config.tseg_1 + t_config.tseg_2),
+                      t_config.quanta_resolution_hz, t_config.tseg_1,
+                      t_config.tseg_2);
+            }
+            fprintf(file,
+                    " tx_io=%i rx_io=%i clkout_io=%i bus_off_io=%i "
+                    "tx_queue_len=%" PRIu32 " rx_queue_len=%" PRIu32
+                    " alerts_enabled=%" PRIu32 " clkout_divider=%" PRIu32
+                    " intr_flags=0x%X\n",
+                    g_config.tx_io, g_config.rx_io, g_config.clkout_io,
+                    g_config.bus_off_io, g_config.tx_queue_len,
+                    g_config.rx_queue_len, g_config.alerts_enabled,
+                    g_config.clkout_divider, g_config.intr_flags);
+          });
 #endif
     }
     break;
