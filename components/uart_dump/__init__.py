027864ed55a10342ae63180940cbfe8cd9540dbd
Anton Sergunov <setosha@gmail.com>
2025-06-13 19:21:00 +0600

    uart logger list

--- components/uart_dump/__init__.py
+++ components/uart_dump/__init__.py
@@ -14,13 +14,14 @@ DEPENDENCIES = ["uart"]
 uart_dump_ns = cg.esphome_ns.namespace("uart_dump")
 UARTDump = uart_dump_ns.class_("UARTDump", cg.Component)
 
-CONFIG_SCHEMA = UART_DEVICE_SCHEMA.extend({
+CONFIG_SCHEMA = cv.ensure_list(UART_DEVICE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(UARTDump),
-})
+}))
 
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    uart = await cg.get_variable(config[CONF_UART_ID])
-    cg.add(var.set_uart(uart))
+async def to_code(configs):
+    for config in configs:
+        var = cg.new_Pvariable(config[CONF_ID])
+        await cg.register_component(var, config)
+        uart = await cg.get_variable(config[CONF_UART_ID])
+        cg.add(var.set_uart(uart))
